ec4c487b9946f35dd186d43965f6dafb9ea20957
rifkiystark <ananda.rifkiy32@gmail.com>
2021-06-08 20:31:27 +0700

    add splashscreen and icon

---
+++ RecheckApps/app/src/main/java/id/B21_CAP0304/RecheckApps/ui/splashscreen/SplashscreenActivity.kt
@@ -0,0 +1,19 @@
+package id.B21_CAP0304.RecheckApps.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import id.B21_CAP0304.RecheckApps.R
+import id.B21_CAP0304.RecheckApps.ui.main.MainActivity
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
+    }
+}
